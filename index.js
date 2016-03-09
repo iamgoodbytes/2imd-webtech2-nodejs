e2442195ca9bd077eada177eba24172002237775
Joris Hens <joris@goodbytes.be>
2016-03-09 12:02:09 +0100

    added the body-parser module

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 app.use('/', require('./routers/index'));
 app.use('/messages', require('./routers/messages'));
+
+// parse application/json 
+app.use(bodyParser.json());
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
