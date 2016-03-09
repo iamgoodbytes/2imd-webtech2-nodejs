05d76de9bbabb64e8f576be1bba126d9bce8db3f
Joris Hens <joris@goodbytes.be>
2016-03-09 11:23:54 +0100

    basic routing added

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/messages', function (req, res) {
+	res.send("GET messages");
+});
+
+app.get('/messages/:id', function (req, res) {
+	var id = req.params.id;
+	res.send("GET message with :id " + id);
+});
+
+app.post('/messages', function (req, res){
+	res.send("POST messages");
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
