5287926978905475921ac2f13261000c0056b2c5
Joris Hens <joris@goodbytes.be>
2016-03-09 12:19:26 +0100

    added mongoose model for messages

--- routers/messages.js
+++ routers/messages.js
@@ -1,6 +1,17 @@
 // access the router object, so that we can add routes to it
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+
+// add one message to the database
+// first, we define a mongoose schema
+var messageSchema = mongoose.Schema({
+	user: String,
+	message: String
+});
+// then we compile this schema into a model
+var Message = mongoose.model('Message', messageSchema);
+
 
 router.get('/', function (req, res) {
 	res.send("GET messages");
@@ -12,7 +23,20 @@ router.get('/:id', function (req, res) {
 });
 
 router.post('/', function (req, res){
-	res.send("POST messages");
+	
+	
+
+	// so that we can save a new instance of this model
+	var newMessage = new Message({
+		user: req.body.user,
+		message: req.body.message
+	});
+	
+	newMessage.save(function (err, message) {
+	  if (err) return console.error(err);
+	  res.send(message);
+	});
+
 });
 
 module.exports = router;
