54f152455d8a186c35da465437a2073df217f76c
Joris Hens <joris@goodbytes.be>
2016-03-09 12:29:50 +0100

    restructuring code into controllers and models

--- routers/messages.js
+++ routers/messages.js
@@ -1,17 +1,7 @@
 // access the router object, so that we can add routes to it
 var express = require('express');
 var router = express.Router();
-var mongoose = require('mongoose');
-
-// add one message to the database
-// first, we define a mongoose schema
-var messageSchema = mongoose.Schema({
-	user: String,
-	message: String
-});
-// then we compile this schema into a model
-var Message = mongoose.model('Message', messageSchema);
-
+var controller = require('../controllers/user')
 
 router.get('/', function (req, res) {
 	res.send("GET messages");
@@ -22,19 +12,6 @@ router.get('/:id', function (req, res) {
 	res.send("GET message with :id " + id);
 });
 
-router.post('/', function (req, res){
-
-	// so that we can save a new instance of this model
-	var newMessage = new Message({
-		user: req.body.user,
-		message: req.body.message
-	});
-	
-	newMessage.save(function (err, message) {
-	  if (err) return console.error(err);
-	  res.send(message);
-	});
-
-});
+router.post('/', controller.create);
 
 module.exports = router;
