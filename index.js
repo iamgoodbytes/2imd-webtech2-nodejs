e3c8048118c4353442ba633262fbd6115bc3155e
Joris Hens <joris@goodbytes.be>
2016-03-09 11:34:58 +0100

    move routes to modules

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+app.use('/', require('./routers/index'));
 
 app.get('/messages', function (req, res) {
 	res.send("GET messages");
