f835149b8ba56337266142cb49939bbbb5fd2be2
Joris Hens <joris@goodbytes.be>
2016-03-09 12:19:14 +0100

    moved body-parser before route definition

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/nodeINTRO');
+
+// parse application/json , define this BEFORE adding routes
+app.use(bodyParser.json());
+
 app.use('/', require('./routers/index'));
 app.use('/messages', require('./routers/messages'));
-
-// parse application/json 
-app.use(bodyParser.json());
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
