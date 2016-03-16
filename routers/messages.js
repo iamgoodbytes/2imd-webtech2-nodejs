ca05248f58f3b178b6f08255b4417a0590f1f191
Joris Hens <joris@goodbytes.be>
2016-03-16 15:38:57 +0100

    renamed user controller to message controller

--- routers/messages.js
+++ routers/messages.js
@@ -1,7 +1,7 @@
 // access the router object, so that we can add routes to it
 var express = require('express');
 var router = express.Router();
-var controller = require('../controllers/user')
+var controller = require('../controllers/message')
 
 // todo, move this to the controller
 router.get('/:id', function (req, res) {
